1f466edd24a04ed158a04ba917d953878dbdf494
Chris Grandin <cgrandin@shaw.ca>
2019-07-10 13:59:00 -0700

    Add download_om

---
+++ R/download-om.R
@@ -0,0 +1,29 @@
+#' Download an operating model from the DLMtool website
+#'
+#' @param file The name of the file (should be .rds)
+#' @param web_file Like this: "Pacific_Ocean_Perch_QC_BC_DFO/OM.rdata" See webpage for listing
+#' @param base_url The URL to find the web_file on
+#' @param method Download method. It seems that on "wget" works on MS Windows
+#' @param overwrite TRUE or FALSE to overwrite file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' download_om("pop", "Pacific_Ocean_Perch_QC_BC_DFO/OM.rdata")
+download_om <- function(file = "default-download-om.rds",
+                        web_file,
+                        base_url = "http://www.datalimitedtoolkit.org/Case_Studies_Table/",
+                        method = "wget",
+                        overwrite = FALSE){
+  nc <- nchar(file)
+  if(tolower(stringi::stri_sub(file, from = nc - 3, to = nc)) != ".rds"){
+    file <- paste0(file, ".rds")
+  }
+
+  if(overwrite | !file.exists(file)){
+    download.file(paste0(base_url, web_file),
+                  file,
+                  method = method)
+  }
+}
