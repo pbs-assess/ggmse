70dd775d5f363128dfc4631674c5c4af8d479b3c
Sean Anderson <sean@seananderson.ca>
2019-07-16 11:49:56 -0700

    Pass on extra arguments to spider_web

--- R/figures-spider.R
+++ R/figures-spider.R
@@ -3,6 +3,7 @@
 #' @param mse_obj A DLMtool object of class [MSE-class]
 #' @param pm_list A list of strings defining valid Performance metrics
 #' @param palette A palette color as recognized by [ggplot2::scale_color_brewer()]
+#' @param ... Other arguments to pass to [ggspider::spider_web()].
 #'
 #' @return A ggplot object
 #' @importFrom ggspider spider_web
@@ -20,7 +21,7 @@ spider <- function(mse_obj,
                      "PNOF",
                      "AAVY"
                    ),
-                   palette = "Set2") {
+                   palette = "Set2", ...) {
   x <- eval_pm(mse_obj, pm_list)
   ggspider::spider_web(x,
     "mp",
@@ -28,6 +29,7 @@ spider <- function(mse_obj,
     "prob",
     palette = palette,
     leg_main_title = "Management Procedure",
-    leg_lty_title = "MP type"
+    leg_lty_title = "MP type",
+    ...
   )
 }
