35dcaa091217b7349992bf31d8194dbaac57df2b
Sean Anderson <sean@seananderson.ca>
2019-12-20 12:24:08 -0500

    Separate data preparation from projection plotting

--- R/figures-projections.R
+++ R/figures-projections.R
@@ -31,7 +31,7 @@
 #' plot_projection_ts(x)
 #' plot_projection_ts(x, type = "SSB")
 plot_projection_ts <- function(object,
-                               type = c("F_FMSY", "B_BMSY"),
+                               type = c("SSB", "FM"),
                                n_samples = 3,
                                this_year = 2018,
                                probs = c(0.1, 0.5),
@@ -40,96 +40,8 @@ plot_projection_ts <- function(object,
                                catch_reference = 5,
                                clip_ylim = NULL,
                                seed = 42) {
-  if (!class(object) != "mse") {
-    stop(
-      "`object` must be a DLMtool object of class `mse`",
-      "that was created by running `DLMtool::runMSE()`."
-    )
-  }
-  .proj_years <- seq(this_year + 1, this_year + object@proyears)
-  years_df <- data.frame(
-    year = seq_len(object@proyears), real_year = .proj_years,
-    stringsAsFactors = FALSE
-  )
-
-  mps <- data.frame(
-    mp = seq_along(object@MPs),
-    mp_name = object@MPs, stringsAsFactors = FALSE
-  )
-
-  ts_data <- list()
-  for (i in seq_along(type)) {
-    ts_data[[i]] <- slot(object, type[i]) %>%
-      reshape2::melt() %>%
-      dplyr::rename(
-        iter = .data$Var1, mp = .data$Var2,
-        value = .data$value, year = .data$Var3
-      ) %>%
-      dplyr::left_join(mps, by = "mp") %>%
-      dplyr::mutate(Type = type[i])
-  }
-  ts_data <- dplyr::bind_rows(ts_data)
-  ts_data <- dplyr::left_join(ts_data, years_df, by = "year")
-  iters <- dim(object@SSB)[1]
-  ts_data$iter <- rep(seq_len(iters), length(unique(ts_data$Type))) # parallel messed this up
-
-  # --------------
-  # historical
-
-  n_hist_years <- dim(object@SSB_hist)[3]
-  .hist_years <- seq(this_year - n_hist_years + 1, this_year)
-  years_df <- data.frame(
-    year = seq_len(n_hist_years), real_year = .hist_years,
-    stringsAsFactors = FALSE
-  )
-  hist_data <- list()
-
-  for (i in seq_along(type)) {
-    hist_data[[i]] <- slot(object, paste0(type[i],
-      ifelse(type[i] == "C", "B_hist", "_hist"))) %>%
-      apply(c(1, 3), if(type[i] == "FM") max else sum) %>%
-    reshape2::melt() %>%
-      dplyr::rename(
-        iter = .data$Var1,
-        value = .data$value, year = .data$Var2
-      ) %>%
-      dplyr::mutate(Type = type[i])
-  }
-  hist_data <- dplyr::bind_rows(hist_data)
-  hist_data$iter <- rep(seq_len(iters), length(unique(hist_data$Type))) # parallel messed this up
-  hist_data <- dplyr::left_join(hist_data, years_df, by = "year")
-  hist_data2 <- do.call("rbind",
-    replicate(length(mps$mp), hist_data, simplify = FALSE))
-  hist_data2[["mp_name"]] <- rep(mps$mp_name, each = nrow(hist_data))
-
-  ts_data <- bind_rows(ts_data, hist_data2)
-
-  ref_ssb <- data.frame(ref = object@Misc$MSYRefs$Refs$SSBMSY, iter = seq_len(iters), Type = "SSB", stringsAsFactors = FALSE)
-  ref_msy <- data.frame(ref = 1, iter = seq_len(iters), Type = "C", stringsAsFactors = FALSE)
-  ref_f <- data.frame(ref = object@Misc$MSYRefs$Refs$FMSY, iter = seq_len(iters), Type = "FM", stringsAsFactors = FALSE)
-  refs <- bind_rows(ref_ssb, ref_msy) %>%
-    bind_rows(ref_f)
-
-  ts_data <- left_join(ts_data, refs, by = c("iter", "Type")) %>%
-    mutate(value = value / ref)
-
-  type[type == "SSB"] <- "B_BMSY"
-  type[type == "FM"] <- "F_FMSY"
-  type[type == "C"] <- "Catch"
-  ts_data$Type[ts_data$Type == "FM"] <- "F_FMSY"
-  ts_data$Type[ts_data$Type == "SSB"] <- "B_BMSY"
-  ts_data$Type[ts_data$Type == "C"] <- "Catch"
-
-  quantiles <- ts_data %>%
-    dplyr::group_by(.data$mp_name, .data$real_year, .data$Type) %>%
-    dplyr::summarize(
-      median_value = median(.data$value),
-      l = quantile(.data$value, probs = 1 - probs[2] / 2),
-      u = quantile(.data$value, probs = probs[2] / 2),
-      m = quantile(.data$value, probs = 0.50),
-      ll = quantile(.data$value, probs = 1 - probs[1] / 2),
-      uu = quantile(.data$value, probs = probs[1] / 2)
-    )
+  ts_data <- get_ts(object = object, type = type, this_year = this_year)
+  quantiles <- get_ts_quantiles(ts_data, probs = probs)
 
   set.seed(seed)
   sampled_ids <- sample(unique(ts_data$iter), size = n_samples)
@@ -191,4 +103,105 @@ plot_projection_ts <- function(object,
     geom_vline(xintercept = this_year, lty = 2, alpha = 0.3)
   g
 
+}
+
+
+get_ts <- function(object,
+  type = c("SSB", "FM"),
+  this_year = 2018) {
+  if (!class(object) != "mse") {
+    stop(
+      "`object` must be a DLMtool object of class `mse`",
+      "that was created by running `DLMtool::runMSE()`."
+    )
+  }
+  .proj_years <- seq(this_year + 1, this_year + object@proyears)
+  years_df <- data.frame(
+    year = seq_len(object@proyears), real_year = .proj_years,
+    stringsAsFactors = FALSE
+  )
+
+  mps <- data.frame(
+    mp = seq_along(object@MPs),
+    mp_name = object@MPs, stringsAsFactors = FALSE
+  )
+
+  ts_data <- list()
+  for (i in seq_along(type)) {
+    ts_data[[i]] <- slot(object, type[i]) %>%
+      reshape2::melt() %>%
+      dplyr::rename(
+        iter = .data$Var1, mp = .data$Var2,
+        value = .data$value, year = .data$Var3
+      ) %>%
+      dplyr::left_join(mps, by = "mp") %>%
+      dplyr::mutate(Type = type[i])
+  }
+  ts_data <- dplyr::bind_rows(ts_data)
+  ts_data <- dplyr::left_join(ts_data, years_df, by = "year")
+  iters <- dim(object@SSB)[1]
+  ts_data$iter <- rep(seq_len(iters), length(unique(ts_data$Type))) # parallel messed this up
+
+  # --------------
+  # historical
+
+  n_hist_years <- dim(object@SSB_hist)[3]
+  .hist_years <- seq(this_year - n_hist_years + 1, this_year)
+  years_df <- data.frame(
+    year = seq_len(n_hist_years), real_year = .hist_years,
+    stringsAsFactors = FALSE
+  )
+  hist_data <- list()
+
+  for (i in seq_along(type)) {
+    hist_data[[i]] <- slot(object, paste0(type[i],
+      ifelse(type[i] == "C", "B_hist", "_hist"))) %>%
+      apply(c(1, 3), if(type[i] == "FM") max else sum) %>%
+      reshape2::melt() %>%
+      dplyr::rename(
+        iter = .data$Var1,
+        value = .data$value, year = .data$Var2
+      ) %>%
+      dplyr::mutate(Type = type[i])
+  }
+  hist_data <- dplyr::bind_rows(hist_data)
+  hist_data$iter <- rep(seq_len(iters), length(unique(hist_data$Type))) # parallel messed this up
+  hist_data <- dplyr::left_join(hist_data, years_df, by = "year")
+  hist_data2 <- do.call("rbind",
+    replicate(length(mps$mp), hist_data, simplify = FALSE))
+  hist_data2[["mp_name"]] <- rep(mps$mp_name, each = nrow(hist_data))
+
+  ts_data <- bind_rows(ts_data, hist_data2)
+
+  ref_ssb <- data.frame(ref = object@Misc$MSYRefs$Refs$SSBMSY, iter = seq_len(iters),
+    Type = "SSB", stringsAsFactors = FALSE)
+  ref_msy <- data.frame(ref = 1, iter = seq_len(iters), Type = "C", stringsAsFactors = FALSE)
+  ref_f <- data.frame(ref = object@Misc$MSYRefs$Refs$FMSY, iter = seq_len(iters), Type = "FM", stringsAsFactors = FALSE)
+  refs <- bind_rows(ref_ssb, ref_msy) %>%
+    bind_rows(ref_f)
+
+  ts_data <- left_join(ts_data, refs, by = c("iter", "Type")) %>%
+    mutate(value = value / ref)
+
+  type[type == "SSB"] <- "B_BMSY"
+  type[type == "FM"] <- "F_FMSY"
+  type[type == "C"] <- "Catch"
+  ts_data$Type[ts_data$Type == "FM"] <- "F_FMSY"
+  ts_data$Type[ts_data$Type == "SSB"] <- "B_BMSY"
+  ts_data$Type[ts_data$Type == "C"] <- "Catch"
+  ts_data
+}
+
+get_ts_quantiles <- function(x, probs = c(0.1, 0.5)) {
+  x %>%
+    dplyr::group_by(.data$mp_name, .data$real_year, .data$Type) %>%
+    dplyr::summarize(
+      median_value = median(.data$value),
+      l = quantile(.data$value, probs = 1 - probs[2] / 2),
+      u = quantile(.data$value, probs = probs[2] / 2),
+      m = quantile(.data$value, probs = 0.50),
+      ll = quantile(.data$value, probs = 1 - probs[1] / 2),
+      uu = quantile(.data$value, probs = probs[1] / 2)
+    ) %>%
+    ungroup()
 }
