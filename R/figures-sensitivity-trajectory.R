506673796626235e91c4ca51b1faa2ed11b3bcdc
Sean Anderson <sean@seananderson.ca>
2020-01-14 17:28:55 -0800

    Add trajectory sensitivity plot

---
+++ R/figures-sensitivity-trajectory.R
@@ -0,0 +1,102 @@
+#' Sensitivity trajectory plot
+#'
+#' Plot sensitivity of performance metrics to OM slots across iterations
+#' as trajectories.
+#'
+#' @param object A DLMtool MSE object.
+#' @param type Whether to make a B/Bmsy or F/Fmsy projection plot.
+#' @param mp A character vector of MPs to plot (must be included in the MSE
+#'   object). Defaults to all.
+#' @param slots A character vector of OM slots to plot. Will be plotted in this
+#'   order. Set `slots = "all"` to plot all available OM and observation slots.
+#'
+#' @return
+#' A ggplot object
+#' @export
+#'
+#' @examples
+#' library(DLMtool)
+#' om@nsim <- 10
+#' x <- runMSE(om, MPs = c("AvC", "CC1"))
+#' plot_sensitivity_trajectory(x)
+plot_sensitivity_trajectory <- function(object, type = c("B_BMSY", "F_FMSY"), mp = object@MPs,
+                             slots = c("D", "hs", "M", "ageM", "L50", "Linf", "K", "Isd")) {
+
+  type <- match.arg(type)
+  if (class(object) != "MSE")
+    stop("`object` must be class 'MSE'", call. = FALSE)
+  if (any(!slots %in% union(colnames(object@OM), colnames(object@Obs))))
+    stop("All `slots` must be valid `object@OM` or `object@Obs` slot names.", call. = FALSE)
+
+  obs <- suppressMessages(reshape2::melt(object@Obs,
+    variable.name = "om_slot", value.name = "om_value"
+  )) %>%
+    mutate(iter = rep(seq_len(dim(object@Obs)[1]), dim(object@Obs)[2])) %>%
+    mutate(slot_type = "Obs")
+  om <- suppressMessages(reshape2::melt(object@OM,
+    id.vars = NULL, variable.name = "om_slot", value.name = "om_value"
+  )) %>%
+    mutate(iter = rep(seq_len(dim(object@OM)[1]), dim(object@OM)[2])) %>%
+    mutate(slot_type = "OM")
+  om <- suppressWarnings(bind_rows(obs, om))
+
+  dat_ts <- gfdlm:::get_ts(object)
+  dat <- dplyr::inner_join(om, dat_ts, by = "iter")
+
+  if (slots[[1]] == "all" && length(slots) == 1L)
+    slots <- union(colnames(object@OM), colnames(object@Obs))
+
+  dat <- dat %>%
+    dplyr::filter(Type == type) %>%
+    dplyr::filter(mp %in% mp, om_slot %in% slots) %>%
+    group_by(om_slot) %>%
+    mutate(om_value_upr = quantile(om_value, probs = 0.666, na.rm = TRUE)) %>%
+    mutate(om_value_lwr = quantile(om_value, probs = 0.333, na.rm = TRUE)) %>%
+    dplyr::filter(om_value < om_value_lwr | om_value > om_value_upr) %>%
+    mutate(om_value_group = ifelse(om_value < om_value_lwr, "Lower", "Upper"))
+
+  probs <- 0.3
+  dat_summarized <- dat %>%
+    dplyr::group_by(om_value_group, om_slot, mp_name, real_year, Type) %>%
+    dplyr::summarize(
+      l = quantile(.data$value, probs = 1 - probs[1] / 2),
+      u = quantile(.data$value, probs = probs[1] / 2),
+      m = quantile(.data$value, probs = 0.5)) %>%
+    ungroup()
+
+  ylab <- if (type == "B_BMSY") expression(B/B[MSY]) else expression(F/F[MSY])
+  pal <- RColorBrewer::brewer.pal(3, "Set2")[c(2, 3)]
+  dat_summarized %>%
+    mutate(om_slot = factor(om_slot, levels = slots)) %>%
+    ggplot2::ggplot(aes(real_year, m)) +
+    ggplot2::geom_ribbon(mapping = aes(ymin = l, ymax = u, fill = om_value_group), alpha = 0.5) +
+    ggplot2::geom_line(mapping = aes(colour = om_value_group)) +
+    ggplot2::facet_grid(mp_name ~ om_slot) +
+    gfplot::theme_pbs() +
+    ggplot2::labs(x = "Year", y = ylab,
+      colour = "OM\nvalue\nthird", fill = "OM\nvalue\nthird") +
+    geom_vline(xintercept = 2018, lty = 2, colour = "grey50") +
+    geom_hline(yintercept = 1, lty = 2, colour = "grey50") +
+    scale_color_manual(values = c("Lower" = pal[1], "Upper" = pal[2])) +
+    scale_fill_manual(values = c("Lower" = pal[1], "Upper" = pal[2]))
+
+  # range01 <- function(x) (x - min(x)) / (max(x) - min(x))
+  #  %>%
+  #   mutate(om_value = range01(om_value)) %>%
+  #   mutate(om_value = om_value - 0.5) %>%
+  #   ungroup() %>%
+  #   mutate(om_slot = factor(om_slot, levels = slots)) %>%
+  #   ggplot2::ggplot(aes(real_year, value)) +
+  #   ggplot2::geom_line(alpha = 0.4, lwd = 0.5,
+  #     mapping = aes(colour = om_value, group = iter)) +
+  #   ggplot2::facet_grid(mp_name ~ om_slot, scales = "free_y") +
+  #   gfplot::theme_pbs() +
+  #   ggplot2::labs(x = "Year", y = expression(B/B[MSY])) +
+  #   # ggplot2::scale_color_viridis_c() +
+  #   ggplot2::scale_color_gradient2(low = scales::muted("blue"),
+  #     high = scales::muted("red"), mid = "grey60") +
+  #   geom_vline(xintercept = 2018, lty = 2, colour = "grey50") +
+  #   geom_hline(yintercept = 1, lty = 2, colour = "grey50") +
+  #   guides(colour = FALSE) +
+  #   coord_cartesian(ylim = c(0, 5))
+}
