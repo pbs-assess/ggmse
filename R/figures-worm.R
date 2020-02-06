d4ca6b4b6dffc5ba79be59f84f7e9078bd77e336
Sean Anderson <sean@seananderson.ca>
2020-02-06 14:39:32 -0800

    Update worms for grid

--- R/figures-worm.R
+++ R/figures-worm.R
@@ -1,7 +1,8 @@
 #' Make a Kobe "worm" timeseries plot with uncertainty
 #'
-#' @param object An MSE object from DLMtool
-#' @param this_year This year as a numeric value
+#' @param object A named list of MSE objects from DLMtool.
+#'   Names become scenario names.
+#' @param this_year This year as a numeric value.
 #' @param prob Tail probability for the quantiles. 0.5 refers to an
 #'   interquartile range.
 #' @param include_historical Logical: include the historical time?
@@ -12,48 +13,62 @@
 #' @export
 #'
 #' @examples
-#' plot_worms(mse_example, this_year = 2020)
-#' plot_worms(mse_example, this_year = 2020, prob = 0.8)
-plot_worms <- function(object, this_year, prob = 0.5,
+#' x <- list()
+#' x[[1]] <- mse_example
+#' x[[2]] <- mse_example
+#' names(x) <- c("Scenario 1", "Scenario 2")
+#' plot_worms_grid(x, this_year = 2020)
+plot_worms_grid <- function(object_list, this_year, prob = 0.5,
                        include_historical = TRUE) {
-  ts <- get_ts(object, type = c("SSB", "FM"))
-  ts_quantiles <- get_ts_quantiles(ts, probs = c(prob, prob))
-  # d <- filter(ts_quantiles, real_year >= this_year)
-  d <- ts_quantiles
 
-  now <- filter(ts_quantiles, real_year == this_year)
+  out <- purrr::map(object_list, ~{
+    ts <- get_ts(.x, type = c("SSB", "FM"))
+    ts_quantiles <- get_ts_quantiles(ts, probs = c(prob, prob))
 
-  m <- reshape2::dcast(d, mp_name + real_year ~ Type, value.var = "m") %>%
-    rename(b_m = B_BMSY, f_m = F_FMSY)
-  l <- reshape2::dcast(d, mp_name + real_year ~ Type, value.var = "l") %>%
-    rename(b_l = B_BMSY, f_l = F_FMSY)
-  u <- reshape2::dcast(d, mp_name + real_year ~ Type, value.var = "u") %>%
-    rename(b_u = B_BMSY, f_u = F_FMSY)
-  dd <- left_join(m, l, by = c("mp_name", "real_year")) %>%
-    left_join(u, by = c("mp_name", "real_year"))
+    if (!include_historical) {
+      d <- filter(ts_quantiles, real_year >= this_year)
+    } else {
+      d <- ts_quantiles
+    }
 
-  poly_df <- split(dd, paste(dd$mp_name, dd$real_year)) %>%
-    map_df(~ data.frame(
-      x = c(.$b_m, .$b_l, .$b_m, .$b_u, .$b_m),
-      y = c(.$f_l, .$f_m, .$f_u, .$f_m, .$f_l),
-      real_year = unique(.$real_year),
-      mp_name = unique(.$mp_name), stringsAsFactors = FALSE
-    ))
+    now <- filter(ts_quantiles, real_year == this_year)
 
-  now_m <- reshape2::dcast(now, mp_name + real_year ~ Type, value.var = "m") %>%
-    rename(b_m = B_BMSY, f_m = F_FMSY)
-  end <- filter(ts_quantiles, real_year == max(ts_quantiles$real_year))
-  end_m <- reshape2::dcast(end, mp_name + real_year ~ Type, value.var = "m") %>%
-    rename(b_m = B_BMSY, f_m = F_FMSY)
-  start <- filter(ts_quantiles, real_year == min(ts_quantiles$real_year))
-  start_m <- reshape2::dcast(start, mp_name + real_year ~ Type, value.var = "m") %>%
-    rename(b_m = B_BMSY, f_m = F_FMSY)
-  other <- bind_rows(now_m, end_m) %>%
-    bind_rows(start_m)
+    m <- reshape2::dcast(d, mp_name + real_year ~ Type, value.var = "m") %>%
+      rename(b_m = B_BMSY, f_m = F_FMSY)
+    l <- reshape2::dcast(d, mp_name + real_year ~ Type, value.var = "l") %>%
+      rename(b_l = B_BMSY, f_l = F_FMSY)
+    u <- reshape2::dcast(d, mp_name + real_year ~ Type, value.var = "u") %>%
+      rename(b_u = B_BMSY, f_u = F_FMSY)
+    dd <- left_join(m, l, by = c("mp_name", "real_year")) %>%
+      left_join(u, by = c("mp_name", "real_year"))
+
+    poly_df <- split(dd, paste(dd$mp_name, dd$real_year)) %>%
+      map_df(~ data.frame(
+        x = c(.$b_m, .$b_l, .$b_m, .$b_u, .$b_m),
+        y = c(.$f_l, .$f_m, .$f_u, .$f_m, .$f_l),
+        real_year = unique(.$real_year),
+        mp_name = unique(.$mp_name), stringsAsFactors = FALSE
+      ))
+
+    now_m <- reshape2::dcast(now, mp_name + real_year ~ Type, value.var = "m") %>%
+      rename(b_m = B_BMSY, f_m = F_FMSY)
+    end <- filter(ts_quantiles, real_year == max(ts_quantiles$real_year))
+    end_m <- reshape2::dcast(end, mp_name + real_year ~ Type, value.var = "m") %>%
+      rename(b_m = B_BMSY, f_m = F_FMSY)
+    start <- filter(ts_quantiles, real_year == min(ts_quantiles$real_year))
+    start_m <- reshape2::dcast(start, mp_name + real_year ~ Type, value.var = "m") %>%
+      rename(b_m = B_BMSY, f_m = F_FMSY)
+    other <- bind_rows(now_m, end_m) %>%
+      bind_rows(start_m)
+    list(dd = dd, other = other, poly_df = poly_df)
+  })
+
+  dd <- purrr::map_dfr(out, "dd", .id = "scenario")
+  other <- purrr::map_dfr(out, "other", .id = "scenario")
+  poly_df <- purrr::map_dfr(out, "poly_df", .id = "scenario")
+  if (!include_historical) other <- filter(other, real_year >= this_year)
 
   g <- dd %>%
-    mutate(b_rad = abs(b_u - b_l) / 2) %>%
-    mutate(f_rad = abs(f_u - f_l) / 2) %>%
     ggplot(aes(b_m, f_m, colour = real_year)) +
     geom_polygon(aes(x = x, y = y, fill = real_year, group = real_year),
       data = poly_df, alpha = 0.2, inherit.aes = FALSE, colour = NA
@@ -62,8 +77,9 @@ plot_worms <- function(object, this_year, prob = 0.5,
     geom_path(lwd = 1.0, lineend = "round", linejoin = "bevel") +
     scale_color_viridis_c(option = "C", direction = -1) +
     scale_fill_viridis_c(option = "C", direction = -1) +
-    theme_pbs() + facet_wrap(~mp_name) +
-    coord_fixed(xlim = c(0, 3), ylim = c(0, 3)) +
+    theme_pbs() +
+    ggplot2::facet_grid(scenario ~ mp_name) +
+    coord_fixed(xlim = c(0, 3), ylim = c(0, 3), expand = FALSE) +
     geom_vline(xintercept = c(0.4, 0.8), lty = 2, alpha = 0.2, lwd = 0.5) +
     geom_hline(yintercept = 1, lty = 2, alpha = 0.2, lwd = 0.5) +
     labs(
